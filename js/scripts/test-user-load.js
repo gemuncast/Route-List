304a1dad4871ce98bcd29dade76eabe3aeda97c2
gemuncast <gemuncast@gmail.com>
2020-07-06 23:11:46 -0400

    git status

--- js/scripts/test-user-load.js
+++ js/scripts/test-user-load.js
@@ -1,8 +1,7 @@
 fetch("../js/data/data-base.json").then(function (resp) {
         return resp.json();
     })
-    .then(
-        function (data) {
+    .then(function (data) {
             document.getElementById("id").innerHTML = "id: " + data[0].id
             document.getElementById("name").innerHTML = "name: " + data[0].name
             document.getElementById("age").innerHTML = "age: " + data[0].age
@@ -11,6 +10,6 @@ fetch("../js/data/data-base.json").then(function (resp) {
             document.getElementById("weight").innerHTML = "weight: " + data[0].weight
             document.getElementById("profession").innerHTML = "profession: " + data[0].profession
             document.getElementById("hobby").innerHTML = "hobby: " + data[0].hobby.join(", ")
-            document.getElementById("address").innerHTML = "address: " + data[0].address
+            document.getElementById("address").innerHTML = "address: " + data[0].address.street +" #"+ data[0].address.number + ", " + data[0].address.city 
         });
     
